08acf92f7420c7aa8fe1153de3135bedd0444ada
Sergiy Tolkachov <metatypeman@gmail.com>
2021-01-01 17:55:59 +0200

    Update MyScript.cs

--- Assets/MyScript.cs
+++ Assets/MyScript.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
+[AddComponentMenu("SymOntoClay/MyScript")]//This attribute adds 'MyScript' to component menu. And also It removes '(Srtipt)' from title in object inspector.
 public class MyScript : MonoBehaviour
 {
     public SomeObject SomeObject;
@@ -32,6 +33,8 @@ public class MyScriptEditor : Editor
 
     //public override void OnInspectorGUI()
     //{
+    //    GUILayout.BeginVertical();
 
+    //    GUILayout.EndVertical();
     //}
 }
